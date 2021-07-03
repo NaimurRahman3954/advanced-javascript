82cdca026c3f1d3db20b4ef6de02c1a38f2b7bf9
NaimurRahman3954 <mnrahaman3954@gmail.com>
2021-07-03 19:51:41 +0600

    message

--- map-filter.js
+++ map-filter.js
@@ -2,7 +2,7 @@ const numbers = [3, 4, 5, 6, 7];
 const output = [];
 
 for (let i = 0; i < numbers.length; i++) {
-  const element = array[i];
+  const element = numbers[i];
   const result = element*element;
   output.push(result);
 }
