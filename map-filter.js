74c08d5183fbd5da98d4b12925b3e4b63de1e182
NaimurRahman3954 <mnrahaman3954@gmail.com>
2021-08-14 01:12:20 +0600

    few changes have been made

--- map-filter.js
+++ map-filter.js
@@ -1,18 +1,26 @@
-const numbers = [3, 4, 5, 6, 7];
-// const output = [];
+const numbers = [3, 4, 5, 6, 7]; // we wanna square all the elements and push them to 'output' array.
+const output = [];
 
-// for (let i = 0; i < numbers.length; i++) {
-//   const element = numbers[i];
-//   const result = element*element;
-//   output.push(result);
-// }
+// traditional method
+for (let i = 0; i < numbers.length; i++) {
+  const element = numbers[i];
+  const result = element*element;
+  output.push(result);
+}
 
-// const output = numbers.map(function(element) {
-//   return element* element;
-// })
+// using map
+const output = numbers.map(function(element) {
+  return element* element;
+})
 
-// const output = numbers.map(x => x*x);
+// using map and array function
+const output = numbers.map(x => x*x);
 
-const output = numbers.filter(x => x>5);
+console.log(output);
+
+
+// The filter() method creates a new array with all elements that pass the test implemented by the provided function.
+// using filter
+const output = numbers.filter(x => x>5); // shows numbers that are greater than 5
 
 console.log(output);
