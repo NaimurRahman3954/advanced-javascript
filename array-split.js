6aaa4b8c17bd32483e9736117561e9bd64eb2148
NaimurRahman3954 <mnrahaman3954@gmail.com>
2021-08-14 12:49:36 +0600

    learnt a few JS'
    clear

--- array-split.js
+++ array-split.js
@@ -1,9 +1,15 @@
 const nums = [1,2,3,4,5,6,7,8];
-const part = nums.slice(2,5);
-const removed = nums.splice(2,3);
 
-const together = nums.join(", ")
+// The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified.
+const part = nums.slice(2,3); // shows from 2nd index to before 5th index. (starts from 0)
+
+// The splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place.
+const removed = nums.splice(2,6);   // shows 6 items from 2nd index onwards
+
+// The join() method creates and returns a new string by concatenating all of the elements in an array (or an array-like object), separated by commas or a specified separator string. If the array has only one item, then that item will be returned without using the separator.
+const together = nums.join("_");
 
 console.log(part);
 console.log(removed);
 console.log(nums);   
+console.log(together);
--- break-continue.js
+++ break-continue.js
@@ -1,6 +1,18 @@
 const nums = [1,2,3,4,5,6,7,8,9];
+for (let i = 0; i < nums.length; i++) {
+   if (nums[i]>5){
+      break;
+   }
+   const element = nums[i];
+   console.log(element);
+} 
 
-for (let i = 0; i < nums .length; i++) {
- const element = nums[i];
-  
+const nums2 = [1,-2,3,-4,5,-6,7,-8,9];
+for (let i = 0; i < nums2.length; i++) {
+   // skips negative values
+   if (nums2[i]<0){
+      continue;      
+   }
+   const element2 = nums2[i];
+   console.log(element2);
 } 
